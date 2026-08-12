fb83e48939a7b5cebab7e1b263a8cedff0b74f42
yourskif <99397282+yourskif@users.noreply.github.com>
2026-08-12 19:38:05 +0300

    Configure SQLite infrastructure

--- src/ConferenceRoomBooking.Api/Program.cs
+++ src/ConferenceRoomBooking.Api/Program.cs
@@ -1,9 +1,13 @@
+using ConferenceRoomBooking.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ src/ConferenceRoomBooking.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using ConferenceRoomBooking.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConferenceRoomBooking.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+            return services;
+        }
+    }
+}
