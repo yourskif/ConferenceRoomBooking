66a4dee7300bf89abe67f6dee32c8115a794f0e2
yourskif <99397282+yourskif@users.noreply.github.com>
2026-08-15 13:26:00 +0300

    Add pricing service tests

---
+++ tests/ConferenceRoomBooking.Tests/Services/PricingServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using ConferenceRoomBooking.Application.Services;
+
+namespace ConferenceRoomBooking.Tests.Services
+{
+    public class PricingServiceTests
+    {
+        [Fact]
+        public void CalculateRoomPrice_WhenBookingIsInStandardMorningPeriod_ReturnsBasePrice()
+        {
+            var pricingService = new PricingService();
+
+            var startTime = new DateTime(2026, 8, 20, 10, 0, 0);
+            var endTime = new DateTime(2026, 8, 20, 12, 0, 0);
+
+            var price = pricingService.CalculateRoomPrice(
+                baseHourlyRate: 2000m,
+                startTime: startTime,
+                endTime: endTime);
+
+            Assert.Equal(4000m, price);
+        }
+    }
+}
