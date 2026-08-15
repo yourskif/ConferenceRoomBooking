ffde4400673275433d8a7c49b15891d93a1e2473
yourskif <99397282+yourskif@users.noreply.github.com>
2026-08-15 13:05:40 +0300

    Implement room pricing rules

--- src/ConferenceRoomBooking.Application/Services/PricingService.cs
+++ src/ConferenceRoomBooking.Application/Services/PricingService.cs
@@ -17,10 +17,86 @@
                 throw new ArgumentException("Start time must be earlier than end time.");
             }
 
-            var duration = endTime - startTime;
-            var hours = (decimal)duration.TotalHours;
+            var totalPrice = 0m;
+            var currentTime = startTime;
 
-            return baseHourlyRate * hours;
+            while (currentTime < endTime)
+            {
+                var nextBoundary = GetNextRateBoundary(currentTime);
+                var periodEnd = nextBoundary < endTime ? nextBoundary : endTime;
+
+                var hours = (decimal)(periodEnd - currentTime).TotalHours;
+                var multiplier = GetHourlyRateMultiplier(TimeOnly.FromDateTime(currentTime));
+
+                totalPrice += baseHourlyRate * multiplier * hours;
+
+                currentTime = periodEnd;
+            }
+
+            return totalPrice;
+        }
+
+        private DateTime GetNextRateBoundary(DateTime currentTime)
+        {
+            var currentDate = currentTime.Date;
+            var currentHour = TimeOnly.FromDateTime(currentTime);
+
+            if (currentHour < new TimeOnly(9, 0))
+            {
+                return currentDate.AddHours(9);
+            }
+
+            if (currentHour < new TimeOnly(12, 0))
+            {
+                return currentDate.AddHours(12);
+            }
+
+            if (currentHour < new TimeOnly(14, 0))
+            {
+                return currentDate.AddHours(14);
+            }
+
+            if (currentHour < new TimeOnly(18, 0))
+            {
+                return currentDate.AddHours(18);
+            }
+
+            if (currentHour < new TimeOnly(23, 0))
+            {
+                return currentDate.AddHours(23);
+            }
+
+            throw new ArgumentException("Booking time must be between 06:00 and 23:00.");
+        }
+
+        private decimal GetHourlyRateMultiplier(TimeOnly time)
+        {
+            if (time >= new TimeOnly(6, 0) && time < new TimeOnly(9, 0))
+            {
+                return 0.9m;
+            }
+
+            if (time >= new TimeOnly(9, 0) && time < new TimeOnly(12, 0))
+            {
+                return 1m;
+            }
+
+            if (time >= new TimeOnly(12, 0) && time < new TimeOnly(14, 0))
+            {
+                return 1.15m;
+            }
+
+            if (time >= new TimeOnly(14, 0) && time < new TimeOnly(18, 0))
+            {
+                return 1m;
+            }
+
+            if (time >= new TimeOnly(18, 0) && time < new TimeOnly(23, 0))
+            {
+                return 0.8m;
+            }
+
+            throw new ArgumentException("Booking time must be between 06:00 and 23:00.");
         }
     }
 }
